904499fa569f47a6151053a8e4b24e2d9b9d7631
Vaibhav <ishuvaibhav@gmail.com>
2016-11-05 05:45:40 -0400

    create the json stuff

--- src/apiImpl.go
+++ src/apiImpl.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 	"github.com/melvinmt/firebase"
 )
 
@@ -19,10 +20,34 @@ func ping (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Working well!")
 }
 
+
+func serveRest(w http.ResponseWriter, r *http.Request){
+	response, err := getJsonResponse()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(response))
+
+}
+
+
+func getJsonResponse() ([]byte, error){
+	doctor := DoctorName{
+		First: "Dr",
+		Last: "Pepper",
+	}
+
+	return json.MarshalIndent(doctor, "", "  ")
+}
+
+
+
+
 func login(w http.ResponseWriter, r *http.Request) {
 	var err error
 
-	url := "https://testing-86363.firebaseio.com/users/test"
+	url := "https://testing-86363.firebaseio.com/users/"
 	ref := firebase.NewReference(url)
 
 	doctor := DoctorName{
